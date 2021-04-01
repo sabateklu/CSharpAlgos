6aa2f8d8503bcc8712546328c0512cc42f2ffd9b
Saba Teklu <saab.teklu@gmail.com>
2021-04-01 11:02:32 -0600

    * .gitignore: initial commit * AlgosCSharp.sln: * Program.cs: * AlgosCSharp.csproj:

---
+++ AlgosCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AlgosCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("trying to push this to github");
+        }
+    }
+}
