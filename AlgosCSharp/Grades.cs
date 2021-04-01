7abb20949f0fcafd9573560b0a9b82ae301f6d38
Saba Teklu <saab.teklu@gmail.com>
2021-04-01 11:14:15 -0600

    * Grades.cs: Grades Class added with Grade Students Method
    * Program.cs: Calling grade students static method and logging results

---
+++ AlgosCSharp/Grades.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgosCSharp
+{
+    public class Grades
+    {
+        public Grades()
+        {
+        }
+
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            // input: an array of integers
+            // output: an array of integers
+            List<int> finalGrades = new System.Collections.Generic.List<int>();
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                if (grades[i] % 5 >= 3 && grades[i] >= 38)
+                {
+                    int newGrade = grades[i] + 5 - grades[i] % 5;
+                    finalGrades.Add(newGrade);
+                }
+                else if (grades[i] % 5 < 3 || grades[i] < 38)
+                {
+                    finalGrades.Add(grades[i]);
+                }
+            }
+            return finalGrades;
+        }
+    }
+}
--- AlgosCSharp/Program.cs
+++ AlgosCSharp/Program.cs
@@ -8,6 +8,17 @@ namespace AlgosCSharp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("trying to push this to github");
+
+            System.Collections.Generic.List<int> grades = (Grades.gradingStudents(new System.Collections.Generic.List<int>()
+                {
+                4, 73, 67, 38, 33
+                }
+                ));
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                Console.WriteLine(grades[i]);
+            }
         }
     }
 }
